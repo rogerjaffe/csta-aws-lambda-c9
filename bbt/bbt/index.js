f5b301b7ee6f6628f937a855f25a84b0e8450424
EC2 Default User <ec2-user@ip-172-31-87-217.ec2.internal>
2019-08-12 19:12:52 +0000

    Finish queries

--- bbt/bbt/index.js
+++ bbt/bbt/index.js
@@ -1,7 +1,35 @@
 const bbt = require('big-bang-theory');
 const moment = require('moment');
+
+/**
+ * Import a CSS style sheet
+ */
 const styles = require('./styles.js');
+
+/**
+ * Generate an HTML template string, embedding the CSS styles
+ * imported above
+ * body: HTML body
+ * returns HTML string
+ */
 const formatHTML = body => `<html><head><style>${styles}</style></head><body>${body}</body></html>`;
+
+/**
+ * Build an HTML table string from individual HTML rows
+ * rows: Array of HTML code representing the rows of the table
+ * returns: Complete HTML document string 
+ */ 
+const buildHTMLTable = rows => {
+  var html = rows.map(ep => {
+    var formattedDate = moment(ep.airdate).format('MMM D, YYYY');  
+    return `<tr><td>${ep.name}</td><td>${ep.season}</td><td>${ep.number}</td><td>${formattedDate}</td><td>${ep.summary}</td></tr>`;
+  });
+  var tableData = html.reduce((acc, row) => acc + row, '');
+  tableData = `<tbody>${tableData}</tbody>`;
+  var header = '<thead><tr><th>Name</th><th>Season</th><th>Episode</th><th>Air date</th><th>Summary</th></tr></thead>';
+  var table = `<table>${header}${tableData}</table>`;
+  return formatHTML(table);
+}
 
 exports.handler = (event, context, callback) => {
 
@@ -10,38 +38,47 @@ exports.handler = (event, context, callback) => {
   }
 
   var sc = 200; // Status code
-  var result = formatHTML("<p>Hello!</p>");
-  
+  var result = "<p>Empty page!</p>";
+
+  // If query parameters exist
   if (event.queryStringParameters) {
+    // Grab the episode array from the database
     var episodes = bbt._embedded.episodes;
+    
+    // Branch to the right processing section based on the requested query
     switch (event.queryStringParameters.type) {
+      // Search by episode name (title)
       case 'name':
         var filteredArray = episodes.filter(episode => episode.name.toLowerCase().indexOf(event.queryStringParameters.name.toLowerCase()) >= 0)
-        var html = filteredArray.map(ep => {
-          var formattedDate = moment(ep.airdate).format('MMM D, YYYY');  
-          return `<tr><td>${ep.name}</td><td>${ep.season}</td><td>${ep.number}</td><td>${formattedDate}</td><td>${ep.summary}</td></tr>`;
-        });
-        var tableData = html.reduce((acc, row) => acc + row);
-        tableData = `<tbody>${tableData}</tbody>`;
-        var header = '<thead><tr><th>Name</th><th>Season</th><th>Episode</th><th>Air date</th><th>Summary</th></tr></thead>';
-        var table = `<table>${header}${tableData}</table>`;
-        result = formatHTML(table);
+        result = buildHTMLTable(filteredArray);
         break;
 
+      // Search by season and episode number
       case 'episode':
-        result = formatHTML('<p>episode</p>');
+        var filteredArray = episodes.filter(episode => episode.season+'' === event.queryStringParameters.season && episode.number+'' === event.queryStringParameters.episode);
+        result = buildHTMLTable(filteredArray);
         break;
 
-      case 'description':
-        result = formatHTML('<p>description</p>');
+      // Search the episodes for summaries that contain the provided string
+      case 'summary':
+        var filteredArray = episodes.filter(episode => episode.summary.toLowerCase().indexOf(event.queryStringParameters.summary.toLowerCase()) >= 0)
+        result = buildHTMLTable(filteredArray);
         break;
 
+      // Search for episodes with dates between from and to parameters
       case 'dates':
-        result = formatHTML('<p>dates</p>');
+        var filteredArray = episodes.filter(episode => event.queryStringParameters.from <= episode.airdate && episode.airdate <= event.queryStringParameters.to);
+        result = buildHTMLTable(filteredArray);
         break;
 
+      // Search for episodes that contain the provided string in the provided database field.  Display the 
+      // number of records that were found
       case 'count':
-        result = formatHTML('<p>count</p>');
+        var field = event.queryStringParameters.field;
+        var searchString = event.queryStringParameters.string;
+        var filteredArray = episodes.filter(episode => (episode[field]+'').toLowerCase().indexOf((searchString+'').toLowerCase()) >= 0);
+        var html = `<p>Target field: ${field}</p><p>Target string: ${searchString}</p><p>There are ${filteredArray.length} records containing the target string</p>`;
+        result = formatHTML(html);
         break;
 
       default:
